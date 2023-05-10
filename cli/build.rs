732a1a7181c7828ed8e9aad9c3bd9dcb8ccb669b
hronro <hronro@gmail.com>
2023-05-10 21:12:36 +0800

    feat(cli): allow using pre-built libjd using the `LIBJD_PATH` env variable

--- cli/build.rs
+++ cli/build.rs
@@ -8,36 +8,42 @@ fn main() {
         p.push("core");
         p
     };
-    let out_dir = {
-        let mut p = core_dir.clone();
-        p.push("zig-out");
-        p.push("lib");
-        p
-    };
 
-    let zig_build_option = if cfg!(debug_assertions) {
-        vec!["build"]
+    if let Ok(libjd_path) = env::var("LIBJD_PATH") {
+        println!("cargo:rustc-link-search=native={}", libjd_path);
+        println!("cargo:rustc-link-lib=static=jd");
     } else {
-        vec!["build", "-Doptimize=ReleaseSmall"]
-    };
+        let out_dir = {
+            let mut p = core_dir.clone();
+            p.push("zig-out");
+            p.push("lib");
+            p
+        };
 
-    if cfg!(debug_assertions) {
-        println!("Debugging enabled");
-    } else {
-        println!("Debugging disabled");
+        let zig_build_option = if cfg!(debug_assertions) {
+            vec!["build"]
+        } else {
+            vec!["build", "-Doptimize=ReleaseSmall"]
+        };
+
+        if cfg!(debug_assertions) {
+            println!("Debugging enabled");
+        } else {
+            println!("Debugging disabled");
+        }
+
+        if !Command::new("zig")
+            .args(zig_build_option)
+            .current_dir(core_dir)
+            .status()
+            .unwrap()
+            .success()
+        {
+            panic!("Failed to build core");
+        }
+
+        println!("cargo:rustc-link-search=native={}", out_dir.display());
+        println!("cargo:rustc-link-lib=static=jd");
+        println!("cargo:rerun-if-changed=../core/src");
     }
-
-    if !Command::new("zig")
-        .args(zig_build_option)
-        .current_dir(core_dir)
-        .status()
-        .unwrap()
-        .success()
-    {
-        panic!("Failed to build core");
-    }
-
-    println!("cargo:rustc-link-search=native={}", out_dir.display());
-    println!("cargo:rustc-link-lib=static=jd");
-    println!("cargo:rerun-if-changed=../core/src");
 }
