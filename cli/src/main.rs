6fade152ad44bbc0ebd9ef64c4bba6a48c51cfb0
hronro <hronro@gmail.com>
2023-05-10 22:45:43 +0800

    feat(cli): add new option `--no-newline`

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,23 +9,45 @@ mod core;
 mod debug_mode;
 mod renderer;
 
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    // skip self
-    let args = &args[1..];
+#[derive(Default)]
+struct CliOptions {
+    no_newline: bool,
+    debug_mode: bool,
+}
+impl CliOptions {
+    pub fn from_args(args: env::Args) -> Self {
+        let mut options = Self::default();
+        // skip self
+        for arg in args.skip(1) {
+            match arg.as_str() {
+                "--no-newline" => options.no_newline = true,
+                "--debug" => options.debug_mode = true,
+                _ => {}
+            }
+        }
+        options
+    }
+}
 
-    if args.iter().any(|arg| arg == "--debug") {
+fn main() -> std::io::Result<()> {
+    let cli_options = CliOptions::from_args(env::args());
+
+    if cli_options.debug_mode {
         let mut stdout = io::stdout();
         debug_mode::debug_mode(&mut stdout)?;
     } else {
-        let mut stdout = io::stdout();
+        let mut stderr = io::stderr();
 
         let (width, height) = size()?;
 
         let r = renderer::Renderer::new((width, height));
 
-        if let Some(output) = r.render(&mut stdout)? {
-            println!("{output}");
+        if let Some(output) = r.render(&mut stderr)? {
+            if cli_options.no_newline {
+                print!("{output}");
+            } else {
+                println!("{output}");
+            }
         }
     }
 
